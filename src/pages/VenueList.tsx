3f05fa8d0f0d5033518038a03caad2333c8822e7
SokhiPiyush <piyushsokhirps_cse20@its.edu.in>
2024-06-17 12:11:10 +0530

    added filter in venue

--- src/pages/VenueList.tsx
+++ src/pages/VenueList.tsx
@@ -1,4 +1,4 @@
-import React, { useState  , useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import VenueCard from '../components/VenueCard';
 import NavBar from '../components/navbar';
 import RelatedArticles from '../components/RelatedArticles';
@@ -9,18 +9,15 @@ import TopFilter from '../components/TopFilter';
 import FilterBar from '../components/FilterBar';
 import { mockVenues } from '../data';
 
-
-
 function VenueList() {
   const { data, error, isLoading } = useAllVenueQuery("");
   const [allvenue, setAllVenues] = useState<Venue[]>([]);
 
- 
   useEffect(() => {
     if (data) {
       setAllVenues(data.data.venues);
     }
-  }, [data])
+  }, [data]);
 
   if (error) {
     return <h1>Error while loading data</h1>;
@@ -29,37 +26,40 @@ function VenueList() {
   if (isLoading) {
     return <h1>Loading</h1>;
   }
-console.log("all venues",allvenue)
+
+  console.log("all venues", allvenue);
 
   return (
     <div className="flex flex-col min-h-screen">
       <NavBar />
-      {/* <TopFilter/> */}
-      <div className="flex-grow container mx-auto px-4 py-8">
-        <div className="grid grid-cols-1">
-        {/* <FilterBar venues={mockVenues} /> */}
-
-          <div>
-          {
-          allvenue.length > 0 ? allvenue.map(( venue , index) =>(
-            <VenueCard
-            key={index}
-              venue={{
-                name: venue?.businessName,
-                location: venue.city,
-                maxGuests: venue.guestCapacity,
-                contact: venue.phone,
-                description: venue.summary,
-                vegPrice: 20,
-                nonVegPrice: 30,
-                images: venue.images,
-                id:venue._id
-              }}
-              />
-          )) : <div>No Vneue found</div>
-        }
+      <TopFilter />
+      <div className="flex flex-grow container mx-auto">
+        <div className="w-1/4 ">
+          <FilterBar venues={mockVenues} />
+        </div>
+        <div className="w-3/4">
+          <div className="grid grid-cols-1 gap-4">
+            {allvenue.length > 0 ? (
+              allvenue.map((venue, index) => (
+                <VenueCard
+                  key={index}
+                  venue={{
+                    name: venue?.businessName,
+                    location: venue.city,
+                    maxGuests: venue.guestCapacity,
+                    contact: venue.phone,
+                    description: venue.summary,
+                    vegPrice: 20,
+                    nonVegPrice: 30,
+                    images: venue.images,
+                    id: venue._id,
+                  }}
+                />
+              ))
+            ) : (
+              <div>No Venue found</div>
+            )}
           </div>
-
         </div>
       </div>
       <div className="mt-8">
